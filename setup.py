2789e0c3bf1ed7d0e72854bd79c491c920c614cc
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2024-06-10 14:44:18 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='DeepSeaProbLog',
+    version='1.0',
+    packages=['deepsea'],
+    package_dir={
+        'deepsea': '.',
+    },
+    author='Lennert De Smet, Pedro Zuidberg Dos Martires, Robin Manhaeve, Giuseppe Marra, Angelika Kimmig, Luc De Raedt'
+)
