f11ad8077fd816dbdb9f6c4f3f9afd3b5d040d44
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2024-11-22 12:27:54 +0000

    small hack to use it as dependency (pls lennert don't do this in future projects :) it's not a good style)

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,2 +1,9 @@
+# add parent directory to sys.path
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
 from engines.engine import Engine
 from engines.exact_engine import ExactEngine
--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(
     name='DeepSeaProbLog',
     version='1.0',
-    packages=['deepsea'],
+    packages=['deepsea', 'deepsea.examples', 'deepsea.engines', 'deepsea.semiring', 'deepsea.utils'],
     package_dir={
         'deepsea': '.',
+        'deepsea.examples': './examples',
+        'deepsea.engines': './engines',
+        'deepsea.semiring': './semiring',
+        'deepsea.utils': './utils',
     },
     author='Lennert De Smet, Pedro Zuidberg Dos Martires, Robin Manhaeve, Giuseppe Marra, Angelika Kimmig, Luc De Raedt',
     license_files = ('LICENSE',),
